b5d0617ebf9e9e0060d5a27c56cc0e87c62d6a71
hodurang2 <cchc0427@gmail.com>
2023-07-20 12:48:35 +0900

    20230720

---
+++ 07_Class_Method/src/ex07_constructor/MainWrapper.java
@@ -0,0 +1,25 @@
+package ex07_constructor;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+    
+    // Computer myCom 객체 선언
+    Computer myCom;
+    
+    // Computer myCom 객체 생성
+    myCom = new Computer(); // public Computer(){}생성자가 호출된다.
+
+    myCom.setModel("갤럭시북");
+    System.out.println("myCom: " + myCom.getModel());
+    
+    // Computer yourCom 객체 선언
+    Computer yourCom;
+    
+    // Computer youtCon 객체 생성
+    yourCom = new Computer("맥북"); // public Computer(String model){} 생성자가 호출된다.
+    
+    System.out.println("yourCom: " + yourCom.getModel());
+  }
+
+}
