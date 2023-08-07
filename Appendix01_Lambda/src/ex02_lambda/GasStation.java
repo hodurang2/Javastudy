d26abd497d698ee5bbc3b5943ab1ce6f09a11e29
hodurang2 <cchc0427@gmail.com>
2023-08-07 19:21:17 +0900

    20230807

--- Appendix01_Lambda/src/ex02_lambda/GasStation.java
+++ Appendix01_Lambda/src/ex02_lambda/GasStation.java
@@ -1,5 +1,66 @@
 package ex02_lambda;
 
+/*
+ * 람다 표현식
+ * 1. Anonymous Inner Type 방식의 객체를 생성할 때 사용할 수 있는 새로운 표현식이다.
+ * 2. 추상 메소드가 하나인 인터페이스에서만 람다 표현식을 사용할 수 있다.
+ *    (함수형 인터페이스 : 추상 메소드가 하나인 인터페이스)
+ * 3. 형식
+ *    (매개변수) -> {
+ *      본문;
+ *    };
+ */
+
+/*
+ * 람다 표현식 적용 예시
+ * 1. 파라미터 X, 반환 X
+ *    () -> {
+ *      System.out.println("Hello World");
+ *    };
+ *    
+ *    () -> System.out.println("Hello World"); // 메소드 본문이 1줄이면 중괄호 {} 생략 가능
+ *    
+ * 2. 파라미터 0, 반환 X (파라미터의 타입은 생략한다.)
+ * 
+ *    (name) -> {
+ *      System.out.println("Hello " + name);
+ *    }
+ *    
+ *    (name) -> System.out.println("Hello " + name);
+ *    
+ * 3. 파라미터 X, 반환 O (반환 타입은 생략한다.)
+ * 
+ *    () -> {
+ *      String name = "tom";
+ *      return name;
+ *    }
+ * 
+ *    () -> {
+ *      return "tom";
+ *    }
+ * 
+ *    () -> return "tom";
+ *    
+ *    () -> "tom"; // 메소드에 return만 존재하면 return을 생략한다.
+ *    
+ * 4. 파라미터 O, 반환 O
+ *    
+ *    // 이름을 전달하면 "님"을 붙여서 출력하고,
+ *    // 해당 값 반환하는 함수
+ *    (name) -> {
+ *      String retVal = name + "님"
+ *      System.out.println(retVal);
+ *      return retVal;
+ *    }
+ *    
+ *    // 정수 값을 전달하면 해당 값보다 1이 큰수를 반환하는 함수
+ *   
+ *    (number) -> number+1;
+ *    
+ *    
+ */
+
+
 public class GasStation {
   
   private int totalOil;
